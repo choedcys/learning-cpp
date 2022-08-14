f04fd95e9453cbc7fecc93b0bb29ad4c021ee388
choedcys <choedcys@naver.com>
2022-08-14 22:48:22 +0900

    Initial Commit

---
+++ learning cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  learning cpp
+//
+//  Created by 조영승 on 2022/08/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
