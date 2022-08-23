2898fa324e9639ba75b872c53df2f6d0de08fb3e
choedcys <choedcys@naver.com>
2022-08-23 23:48:21 +0900

    표준템플릿라이브러리 - 1

--- learning cpp/header.h
+++ learning cpp/header.h
@@ -1,12 +1,3 @@
 #include<iostream>
 using namespace std;
 
-class String{
-    int len;
-    char * query;
-public:
-    String();
-    void setQ();
-    ~String();
-    void change();
-};
--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,11 +1,22 @@
-#include<iostream>
-#include"header.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 using namespace std;
 
-int main(){
-    char*query;
-    char temp;
-    String sen;
-    sen.setQ();
-    sen.change();
+int main() {
+    vector<int> v;
+    int sum = 0;
+    vector<int>::iterator ptr;
+    srand((unsigned int)time(NULL));
+    for(int i = 0 ; i < 10 ; i++){
+        v.push_back(1+rand()%100);
+    }
+    cout<<"벡터의 요소: [";
+    for(ptr = v.begin() ; ptr < v.end() ; ptr++){
+        sum += *ptr;
+        cout<<*ptr<<" ";
+    }
+    cout<<"]"<<endl;
+    cout<<"합: "<<sum<<endl;
 }
