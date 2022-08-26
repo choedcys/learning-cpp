65317d216cd04ecd293c8eabec739940914def40
choedcys <choedcys@naver.com>
2022-08-26 20:32:27 +0900

    표준템플릿라이브러리 - 3

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,33 +1,15 @@
 #include <iostream>
 #include <vector>
-template <typename T>
-void print_vector(std::vector<T>& vec) {
-    std::cout << "[ ";
-    for (typename std::vector<T>::iterator itr = vec.begin(); itr != vec.end();++itr) {
-        std::cout << *itr << " ";
-    }
-    std::cout << "]"; }
+using namespace std;
 
-int main() {
-    std::vector<int> vec;
-    vec.push_back(10);
-    vec.push_back(20);
-    vec.push_back(30);
-    vec.push_back(40);
-    vec.push_back(20);
-    std::cout << "처음 벡터 상태" << std::endl;
-    print_vector(vec);
-    std::vector<int>::iterator itr = vec.begin();
-    std::vector<int>::iterator end_itr = vec.end();
-    
-    for (std::vector<int>::size_type i = 0; i != vec.size(); i++) {
-        if (vec[i] == 20) {
-            vec.erase(vec.begin() + i);
-            i--;
-        }
+int main(){
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    vector<int>::iterator it = v.begin();
+    for( ; it != v.end() ; it++){
+        *it *= 2;
+        cout<<*it<<endl;
     }
-
-std::cout << "값이 20 인 원소를 지운다!" << std::endl;
-print_vector(vec);
-        
 }
