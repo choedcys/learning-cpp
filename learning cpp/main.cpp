2fdb4d5660f7bb1873f38acb3b2d8effe53cea61
choedcys <choedcys@naver.com>
2022-09-16 18:15:47 +0900

    sty 3

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -2,14 +2,19 @@
 #include <vector>
 using namespace std;
 
+
+
 int main(){
     vector<int> v;
-    v.push_back(10);
-    v.push_back(20);
-    v.push_back(30);
-    vector<int>::iterator it = v.begin();
-    for( ; it != v.end() ; it++){
-        *it *= 2;
-        cout<<*it<<endl;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    vector<int>::iterator iterator = v.begin();
+    
+    for( ; iterator < v.end() ; iterator++){
+        *iterator *= 2;
+    }
+    for(int i = 0 ; i < 3 ; i++){
+        cout<<v[i]<<endl;
     }
 }
