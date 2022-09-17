ecb69a1daa116c8b356780c60b23655c5b7a7841
choedcys <choedcys@naver.com>
2022-09-17 11:25:39 +0900

    stl6-1

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -2,27 +2,66 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-double calculate(vector<double>t){
-    vector<double>::iterator it = t.begin();
-    double sum = 0.0;
-    sort(t.begin(),t.end());
-    it++;
-    for( ; it < t.end() ; it++)
-        sum += *it;
-    it--;
-    sum -= *it;
-    return sum;
-}
+class Dict{
+public:
+    string eng;
+    string kor;
+};
 
 int main(){
-    vector<double> v;
-    double temp;
-    
-    for(int i = 0 ; i < 10 ; i++){
-        cout<<"점수를 입력하시오: ";
-        cin>>temp;
-        v.push_back(temp);
+    vector<Dict> v;
+    Dict T;
+    string temp1;
+    string temp2;
+    int cursor = 0;
+    vector<Dict>::iterator it;
+    while(cursor != 4){
+        cout<<"1. 단어추가 2. 단어검색 3.단어삭제 4.종료"<<endl<<"=> ";
+        cin>>cursor;
+        if(cursor == 1){
+            cout<<"단어입력: ";
+            cin.get();
+            getline(cin,temp1);
+            cout<<"뜻 입력: ";
+            getline(cin,temp2);
+            T.eng = temp1;
+            T.kor = temp2;
+            v.push_back(T);
+        }
+        else if(cursor == 2){
+            cout<<"검색할 단어: ";
+            cin.get();
+            getline(cin, temp1);
+            it = v.begin();
+            for( ; it < v.end() ; it++){
+                if(temp1 == it->eng){
+                    cout<<"뜻: "<<it->kor<<endl;
+                    break;
+                }
+            }
+            if(it == v.end())
+                cout<<"검색된 단어가 없음"<<endl;
+        }
+        else if(cursor == 3){
+            cout<<"삭제할 단어: ";
+            cin.get();
+            getline(cin, temp1);
+            it = v.begin();
+            for( ; it < v.end() ; it++){
+                if(temp1 == it->eng){
+                    v.erase(it);
+                    break;
+                }
+            }
+            if(it == v.end())
+                cout<<"검색된 단어가 없음"<<endl;
+        }
+        else if(cursor == 4){
+            break;
+        }
+        else{
+            cout<<"잘못된 입력! "<<endl;
+        }
     }
-    cout<<"최종점수: "<<calculate(v);
     return 0;
 }
