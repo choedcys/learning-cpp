e01fb32a44724c04dd0caed453815e44fee62382
choedcys <choedcys@naver.com>
2022-12-23 11:12:54 +0900

    STL알고리즘_1

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -6,19 +6,14 @@ using namespace std;
 
 
 
-struct Tag{
-    string name;
-    int age;
-};
-
-bool func(Tag temp1, Tag temp2){
-    return temp1.age < temp2.age;
-}
 int main(){
-    vector<Tag> T = {{"ccc", 30}, {"aaa", 10}, {"bbb",10}, {"ddd", 40}, {"eee", 50}, {"aaa", 20}};
-    sort(T.begin(), T.end(), func);
-    vector<Tag>::iterator it = T.begin();
-    for( ; it != T.end() ; it++){
-        cout<< it->name<<" "<< it->age<<endl;
+    vector<int> vec = {5,3,1,2,3,4};
+    vector<int>::iterator it = vec.begin();
+    vector<int>::iterator cursor;
+    while(it <= vec.end()){
+        cursor = find(it, vec.end(), 3);
+        it += distance(vec.begin(), cursor);
+        cout<<3<<"은"<<distance(vec.begin(), cursor)+1<<"번째 원소"<<endl;
+        it++;
     }
 }
