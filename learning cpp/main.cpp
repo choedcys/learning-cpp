a465f4501139183cc21ce95f8fd54ecb846dec8b
choedcys <choedcys@naver.com>
2022-12-23 11:28:55 +0900

    STL알고리즘_3

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -2,23 +2,20 @@
 #include <vector>
 #include <algorithm>
 #include "header.h"
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 
-
-bool condition(int temp){
-    if(temp % 400 == 0 || temp % 4 == 0 && temp % 100 != 0)
-        return true;
-    else
-        return false;
+bool even(int temp){
+    return (temp %2 == 0);
 }
-
 int main(){
-    vector<int>year;
-    vector<int>::iterator y;
-    for(int i = 1990 ; i < 2020 ; i++){
-        year.push_back(i);
-    }
-    y =find_if(year.begin(), year.end(), condition);
-    cout<<"1990~2019년 사이의 첫 번째 윤년 : "<< *y<<"년"<<endl;
+    vector<int>vec;
+    srand((unsigned int)time(NULL));
+    for (int i = 0; i < 100; ++i)
+        {
+            vec.push_back(rand());
+        }
+    cout<<count_if(vec.begin(), vec.end(), even);
 }
