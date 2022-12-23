facdfb908ac1bace63dc52d7a74cefb5084ccec2
choedcys <choedcys@naver.com>
2022-12-23 11:02:16 +0900

    STL알고리즘_8

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,53 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <algorithm>
+#include "header.h"
 using namespace std;
 
 
+
+struct Tag{
+    string name;
+    int age;
+};
+
+bool func(Tag temp1, Tag temp2){
+    return temp1.age < temp2.age;
+}
 int main(){
-    map<string, string> mp;
-    string temp1;
-    string temp2;
-    int cursor = 0;
-    while(cursor != 4){
-        cout<<"1. 단어추가 2. 단어검색 3.단어삭제 4.종료"<<endl<<"=> ";
-        cin>>cursor;
-        if(cursor == 1){
-            cout<<"단어입력: ";
-            cin.get();
-            getline(cin,temp1);
-            cout<<"뜻 입력: ";
-            getline(cin,temp2);
-            mp[temp1] = temp2;
-        }
-        else if(cursor == 2){
-            cout<<"검색할 단어: ";
-            cin.get();
-            getline(cin, temp1);
-            if(!mp[temp1].empty()){
-                cout<<"뜻: "<<mp[temp1]<<endl;
-            }
-            else{
-                cout<<"검색할 단어가 사전에 없습니다. "<<endl;
-            }
-        }
-        else if(cursor == 3){
-            cout<<"삭제할 단어: ";
-            cin.get();
-            getline(cin, temp1);
-            if(!mp[temp1].empty()){
-                mp.erase(temp1);
-            }
-            else{
-                cout<<"삭제할 단어가 사전에 없습니다. "<<endl;
-            }
-        }
-        else if(cursor == 4){
-            break;
-        }
-        else{
-            cout<<"잘못된 입력! "<<endl;
-        }
+    vector<Tag> T = {{"ccc", 30}, {"aaa", 10}, {"bbb",10}, {"ddd", 40}, {"eee", 50}, {"aaa", 20}};
+    sort(T.begin(), T.end(), func);
+    vector<Tag>::iterator it = T.begin();
+    for( ; it != T.end() ; it++){
+        cout<< it->name<<" "<< it->age<<endl;
     }
-    return 0;
 }
