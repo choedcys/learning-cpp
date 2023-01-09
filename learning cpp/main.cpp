a08f148b6fc0c234d014184bf3c22e814173b0d1
choedcys <choedcys@naver.com>
2023-01-09 19:40:00 +0900

    펑터_3

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
-class Plus{
+class Equal{
 public:
     int operator()(int a, int b){
-        return a+b;
+        return a == b;
     }
 };
 
 int main(){
-    Plus plus;
-    std::cout<<"plus(10, 20): "<<plus(10,20)<<std::endl;
+    Equal cmp;
+    std::cout<<"pls(5, 5): "<<cmp(5,5)<<std::endl;
+    std::cout<<"cmp.operator()(10, 20): "<<cmp.operator()(10,20)<<std::endl;
+    std::cout<<"Equal()(10, 10): "<<Equal()(10, 10)<<std::endl;
     return 0;
 }
