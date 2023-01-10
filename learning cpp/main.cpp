0b498dd00170f4fd4b298d00d2a4f97dee0dc491
choedcys <choedcys@naver.com>
2023-01-10 18:10:03 +0900

    펑터_문제4

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
+#include <functional>
+#include <vector>
 
-template <typename T>
-class Complex{
-public:
-    Complex(){}
-    T operator()(T temp1,T temp2){
-        return temp1*temp2;
+void print(std::vector<int> temp){
+    std::vector<int>::iterator it = temp.begin();
+    std::cout<<"{ ";
+    for( ; it != temp.end() ; it++){
+        std::cout<<*it<<" ";
     }
-};
+    std::cout<<"}"<<std::endl;
+}
 
 int main(){
-    Complex<int> comp;
-    std::cout<<"2 * 7 = "<<comp(2, 7)<<std::endl;
+    std::vector<int> V1;
+    std::vector<int> V2;
+    std::vector<int> V3;
+    for(int i = 0 ; i < 3 ; i++){
+        V1.push_back((i+1)*10);
+    }
+    for(int i = 0 ; i < 3 ; i++){
+        V2.push_back(i+1);
+    }
+    std::vector<int>::iterator it1 = V1.begin();
+    std::vector<int>::iterator it2 = V2.begin();
+    for(; it1 != V1.end() ; it1++){
+        V3.push_back(std::plus<int>()(*it1, *it2));
+        it2++;
+    }
+    std::cout<<"V1: ";
+    print(V1);
+    std::cout<<"V2: ";
+    print(V2);
+    std::cout<<"V1+V2: ";
+    print(V3);
     return 0;
 }
