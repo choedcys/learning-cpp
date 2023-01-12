b4f1a43bbd6c7485fdd50656116b7a0d2e1099ce
choedcys <choedcys@naver.com>
2023-01-12 13:21:46 +0900

    펑터 문제_7

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -3,27 +3,26 @@
 #include<functional>
 #include<vector>
 
+bool condition(int n1, int n2){
+    if(n1 > n2)
+        return true;
+    else
+        return false;
+}
+
 int main(){
-    std::vector<int> v1;
-    std::vector<int> v2;
-    for(int i = 1 ; i < 4 ; i++){
-        v1.push_back(i);
-        v2.push_back(i*10);
-    }
-    std::vector<int>::iterator it1 = v1.begin();
-    std::vector<int>::iterator it2 = v2.begin();
-    for( ; it1 != v1.end() ; it1++){
-        std::cout << *it1 <<" ";
+    std::vector<int> v = {100, 500, 1020, 1000};
+    std::vector<int>::iterator it;
+    std::cout<<"소트 전: ";
+    for( it = v.begin() ; it < v.end() ; it++ ){
+        std::cout<<*it<<" ";
     }
     std::cout<<std::endl;
-    for( ; it2 != v2.end() ; it2++){
-        std::cout << *it2 <<" ";
+    std::sort(v.begin(),v.end(),condition);
+    std::cout<<"소트 후: ";
+    for( it = v.begin() ; it < v.end() ; it++ ){
+        std::cout<<*it<<" ";
     }
     std::cout<<std::endl;
-    std::transform(v1.begin(), v1.end(), v2.begin(), v1.begin(), std::plus<int>());
-    it1 = v1.begin();
-    for( ; it1 != v1.end() ; it1++){
-        std::cout << *it1 <<" ";
-    }
 }
 
