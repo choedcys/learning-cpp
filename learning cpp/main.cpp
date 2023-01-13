76e76e2a064dfce9930c5482fba84add7d514c2c
choedcys <choedcys@naver.com>
2023-01-13 20:04:13 +0900

    펑터 문제_9

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,103 +1,24 @@
 #include<iostream>
 #include<algorithm>
-#include<functional>
-#include<vector>
-#include<cmath>
-class acc{
-    int sum = 1;
-public:
-    int operator()(int temp){
-        sum *= temp;
-        return sum;
-    }
-};
-int main(){
-    acc obj;
-    std::vector<int> v1 = {10, 20, 30, 40, 50};
-    std::vector<int> v2 = {1, 2, 3, 4, 5};
-    std::vector<int> v3;
-    std::vector<int>::iterator it1 = v1.begin();
-    std::vector<int>::iterator it2 = v2.begin();
-    for( ; it1 != v1.end() ; it1++){
-        v3.push_back(*it1+*it2);
-        it2++;
-    }
-    std::vector<int>::iterator it3 = v3.begin();
-    it1 = v1.begin();
-    it2 = v2.begin();
-    std::cout<<"v1: ";
-    for( ; it1 != v1.end() ; it1++){
-        std::cout<<*it1<<" ";
-    }
-    std::cout<<std::endl;
-    std::cout<<"v2: ";
-    for( ; it2 != v2.end() ; it2++){
-        std::cout<<*it2<<" ";
-    }
-    std::cout<<std::endl;
-    std::cout<<"v3(v1+v2): ";
-    for( ; it3 != v3.end() ; it3++){
-        std::cout<<*it3<<" ";
-    }
-    std::cout<<std::endl;
-    v3 = {};
-    it1 = v1.begin();
-    it2 = v2.begin();
-    for( ; it1 != v1.end() ; it1++){
-        v3.push_back((*it1)*(*it2));
-        it2++;
-    }
-    it3 = v3.begin();
-    std::cout<<"v3(v1*v2): ";
-    for( ; it3 != v3.end() ; it3++){
-        std::cout<<*it3<<" ";
-    }
-    std::cout<<std::endl;
-    v3 = {};
-    it1 = v1.begin();
-    for( ; it1 != v1.end() ; it1++){
-        v3.push_back(-(*it1));
-    }
-    it3 = v3.begin();
-    std::cout<<"v3(-v1): ";
-    for( ; it3 != v3.end() ; it3++){
-        std::cout<<*it3<<" ";
-    }
-    std::cout<<std::endl;
-    v3 = {};
-    it1 = v1.begin();
-    for( ; it1 != v1.end() ; it1++){
-        v3.push_back(abs((*it1)-*(it1+1)));
-    }
-    it3 = v3.begin();
-    std::cout<<"v3(v1 인접 원소와의 차): ";
-    for( ; it3 != v3.end() ; it3++){
-        std::cout<<*it3<<" ";
-    }
-    std::cout<<std::endl;
-    v3 = {};
-    it1 = v1.begin();
-    for( ; it1 != v1.end() ; it1++){
-        v3.push_back(abs((*it1)-*(it1+1)));
-    }
-    it3 = v3.begin();
-    std::cout<<"v3(v1 인접 원소와의 차): ";
-    for( ; it3 != v3.end() ; it3++){
-        std::cout<<*it3<<" ";
-    }
-    std::cout<<std::endl;
-    v3 = {};
-    it1 = v1.begin();
-    for( ; it1 != v1.end() ; it1++){
-        v3.push_back(obj(*it1));
-    }
-    it3 = v3.begin();
-    std::cout<<"v3(v1 원소의 곱 누적): ";
-    for( ; it3 != v3.end() ; it3++){
-        std::cout<<*it3<<" ";
-    }
-    std::cout<<std::endl;
-    std::cout<<"v1 모든 원소의 곱: ";
-    std::cout<<*(v3.end()-1)<<std::endl;
+#include<list>
+
+template <typename T>
+bool compare(int n){
+    return 10 <= n;
 }
 
+int main(){
+    std::list<int> l1 = {10, 100, 5, 4, 70, 2};
+    std::list<int>::iterator it = l1.begin();
+    std::cout<<"삭제 전 list: ";
+    for( ; it != l1.end() ; it++){
+        std::cout<<*it<<" ";
+    }
+    std::cout<<std::endl;
+    l1.erase(std::remove_if(l1.begin(), l1.end(),compare<int>),l1.end());
+    std::cout<<"삭제 후 list: ";
+    for( auto n:l1){
+        std::cout<<n<<" ";
+    }
+    std::cout<<std::endl;
+}
