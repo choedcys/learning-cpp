5dd5d0523483b192af2a2527267bb4dc7839af91
choedcys <choedcys@naver.com>
2023-01-14 22:06:30 +0900

    람다 문제_2

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,17 +1,13 @@
 #include<iostream>
 
 int main(){
-    int a, b;
-    std::cout<<"숫자 2개 입력: ";
-    std::cin>>a>>b;
-    [=](int a, int b){
-        if(a>b){
-            std::cout<<"큰 값: "<<a<<std::endl;
-            std::cout<<"작은 값: "<<b<<std::endl;
+    [](int a){
+        if(a>0){
+            std::cout<<"절대값: "<<a<<std::endl;
         }
         else{
-            std::cout<<"큰 값: "<<b<<std::endl;
-            std::cout<<"작은 값: "<<a<<std::endl;
+            std::cout<<"절대값: "<<-a<<std::endl;
+            
         }
-    }(a,b);
+    }(-109);
 }
