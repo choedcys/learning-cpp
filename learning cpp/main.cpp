9b3e1d48f12af468a913807d1f665d91942472c4
choedcys <choedcys@naver.com>
2023-01-14 16:01:53 +0900

    람다_10

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,31 +1,10 @@
 #include <iostream>
-#include <string>
+#include <functional>
 
 int main(){
-    int result1 = 1;
-    int result2 = 2;
-    int result3 = 3;
-    int result4 = 4;
+    auto f1 = [](int x, int y){return x + y;};
+    std::cout<<f1(2,3)<<std::endl;
+    std::function<int(int, int)> f2 = [](int x, int y){return x+y;};
     
-    std::cout<<"6. 전체 복사, 일부 참조"<<std::endl;
-    
-    
-    [=, &result3, &result4](int x){
-        std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
-        std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
-        std::cout<<"매개변수: "<<x<<std::endl;
-    }(30);
-    std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
-    std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
-    std::cout<<std::endl;
-    
-    std::cout<<"5. 전체 참조, 일부 복사"<<std::endl;
-    [&, result1, result2](int y){
-        std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
-        std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
-        std::cout<<"매개변수: "<<y<<std::endl;
-    }(99);
-    std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
-    std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
-    std::cout<<std::endl;
+    std::cout<<f2(3,4)<<std::endl;
 }
