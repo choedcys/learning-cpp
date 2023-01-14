81b7c3fe76e511844655cd9c705f7f673558c351
choedcys <choedcys@naver.com>
2023-01-14 15:16:14 +0900

    람다_6

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
-
+#include <string>
 
 int main(){
-    int a = 10;
-    int b = 20;
-    int R = [a, &b](int c)->int{
-        b = a + b + c;
-        return b;
-    }(1);
-    std::cout<<"a="<<a<<" b="<<b<<" 결과="<<R<<std::endl;
+    int result1 = 1;
+    int result2 = 2;
+    int result3 = 3;
+    int result4 = 4;
+    
+    std::cout << "3. 전체 복사"<<std::endl;
+    [=](int x){
+        std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
+        std::cout<<"result3, 4: "<<result3<<", "<<result4<<std::endl;
+        std::cout<<"매개변수: "<<x<<std::endl;
+    }(30);
+    std::cout<<std::endl;
+    [&](int y){
+        std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
+        std::cout<<"result3, 4: "<<result3<<", "<<result4<<std::endl;
+        std::cout<<"매개변수: "<<y<<std::endl;
+        result1 += y;
+        result2 += y;
+        result3 += y;
+        result4 += y;
+    }(99);
+    std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
+    std::cout<<"result3, 4: "<<result3<<", "<<result4<<std::endl;
+    return 0;
 }
