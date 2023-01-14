4f0a4133c24cbdddb9a1a4385504fd21dbd8fbed
choedcys <choedcys@naver.com>
2023-01-15 08:43:47 +0900

    람다 문제_5

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,32 +1,18 @@
 #include<iostream>
 #include<vector>
 
-class Even{
-    int NumberOfEven = 0;
-public:
-    int operator()(int num){
-        if(num%2 == 0)
-        NumberOfEven++;
-        return NumberOfEven;
-    }
-    int getNumberOfEven(){
-        return NumberOfEven;
-    }
-};
 int main(){
-    Even obj;
-    int cnt = 0;
-    std::vector<int> v;
-    for(int i = 1 ; i < 11 ; i++){
-        v.push_back(i);
-    }
-    for(int i = 0 ; i < 10 ; i++){
-        obj(v[i]);
-    }
-    std::cout<<"짝수개수(함수객체): "<<obj.getNumberOfEven()<<std::endl;
-    std::for_each(v.begin(), v.end(), [&cnt](int temp){
-       if(temp%2 == 0)
-           cnt++;
+    std::vector<int> v = {34, -43, 56};
+    std::cout<<"변경 전: ";
+    std::for_each(v.begin(), v.end(), [](int temp){
+        std::cout<<temp<<" ";
     });
-    std::cout<<"짝수개수(람다): "<<cnt<<std::endl;
+    std::cout<<std::endl;
+    std::for_each(v.begin(), v.end(), [](int& temp){
+        temp *= -1;
+    });
+    std::cout<<"변경 후: ";
+    std::for_each(v.begin(), v.end(), [](int temp){
+        std::cout<<temp<<" ";
+    });
 }
