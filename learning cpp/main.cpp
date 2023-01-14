040e01aa38b21f8fc5cdd51a47a99799a1281022
choedcys <choedcys@naver.com>
2023-01-14 15:28:24 +0900

    람다_7

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -7,23 +7,25 @@ int main(){
     int result3 = 3;
     int result4 = 4;
     
-    std::cout << "3. 전체 복사"<<std::endl;
-    [=](int x){
+    std::cout<<"6. 전체 복사, 일부 참조"<<std::endl;
+    
+    
+    [=, &result3, &result4](int x){
         std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
-        std::cout<<"result3, 4: "<<result3<<", "<<result4<<std::endl;
+        std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
         std::cout<<"매개변수: "<<x<<std::endl;
     }(30);
+    std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
+    std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
     std::cout<<std::endl;
-    [&](int y){
+    
+    std::cout<<"5. 전체 참조, 일부 복사"<<std::endl;
+    [&, result1, result2](int y){
         std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
-        std::cout<<"result3, 4: "<<result3<<", "<<result4<<std::endl;
+        std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
         std::cout<<"매개변수: "<<y<<std::endl;
-        result1 += y;
-        result2 += y;
-        result3 += y;
-        result4 += y;
     }(99);
     std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
-    std::cout<<"result3, 4: "<<result3<<", "<<result4<<std::endl;
-    return 0;
+    std::cout<<"result1, 2: "<<result1<<", "<<result2<<std::endl;
+    std::cout<<std::endl;
 }
