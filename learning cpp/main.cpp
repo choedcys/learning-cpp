8cfe345825e82595e0baf81a60334232ebc562ca
choedcys <choedcys@naver.com>
2023-01-15 08:28:36 +0900

    람다 문제_4

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,9 +1,32 @@
 #include<iostream>
 #include<vector>
 
+class Even{
+    int NumberOfEven = 0;
+public:
+    int operator()(int num){
+        if(num%2 == 0)
+        NumberOfEven++;
+        return NumberOfEven;
+    }
+    int getNumberOfEven(){
+        return NumberOfEven;
+    }
+};
 int main(){
-    std::vector<std::string> v = {"apple", "banana", "grape", "orange", "kiwi"};
-    std::for_each(v.begin(), v.end(),[](std::string cursor){
-        std::cout<<cursor<<std::endl;
+    Even obj;
+    int cnt = 0;
+    std::vector<int> v;
+    for(int i = 1 ; i < 11 ; i++){
+        v.push_back(i);
+    }
+    for(int i = 0 ; i < 10 ; i++){
+        obj(v[i]);
+    }
+    std::cout<<"짝수개수(함수객체): "<<obj.getNumberOfEven()<<std::endl;
+    std::for_each(v.begin(), v.end(), [&cnt](int temp){
+       if(temp%2 == 0)
+           cnt++;
     });
+    std::cout<<"짝수개수(람다): "<<cnt<<std::endl;
 }
