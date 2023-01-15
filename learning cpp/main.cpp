46ba6fb470bc30aae23d046e94aab011cb73fcaf
choedcys <choedcys@naver.com>
2023-01-15 15:19:26 +0900

    람다 문제_9

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,19 +1,38 @@
 #include<iostream>
-#include<list>
+#include<vector>
+#include<cstdlib>
+#include<ctime>
 
+class Even{
+    int NumberOfEven = 0;
+public:
+    int operator()(int temp){
+        if(temp%2 == 0)
+            NumberOfEven++;
+        return NumberOfEven;
+    }
+    int getNumberOfEven(){
+        return NumberOfEven;
+    }
+};
 
 int main(){
-    int start, end;
-    std::cout<<"시작: ";
-    std::cin>>start;
-    std::cout<<"끝: ";
-    std::cin>>end;
-    [](int start, int end){
-        int sum = 0;
-        for(int i = start ; i <= end ; i++){
-            sum+=i;
-        }
-        std::cout<<start<<" ~ "<<end<<"의 합은: "<<sum;
-        std::cout<<std::endl;
-    }(start, end);
+    Even even;
+    std::vector<int> v1;
+    srand((unsigned int)time(NULL));
+    int size = rand()%100;
+    for (int i = 0; i < size; ++i){
+        v1.push_back(rand()%100);
+    }
+    std::vector<int>::iterator it = v1.begin();
+    for ( ; it != v1.end(); ++it){
+        std::cout<<*it<<"..";
+        if(*it%2 == 0)
+            std::cout<<"짝수"<<std::endl;
+        else
+            std::cout<<"홀수"<<std::endl;
+        even(*it);
+    }
+    std::cout<<"짝수 개수: "<<even.getNumberOfEven()<<std::endl;
+    return 0;
 }
