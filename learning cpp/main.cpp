269df846c38731df0be3c6ac353b7a8fbabc374b
choedcys <choedcys@naver.com>
2023-01-15 15:36:01 +0900

    STL알고리즘_8

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,38 +1,28 @@
 #include<iostream>
 #include<vector>
-#include<cstdlib>
-#include<ctime>
 
-class Even{
-    int NumberOfEven = 0;
-public:
-    int operator()(int temp){
-        if(temp%2 == 0)
-            NumberOfEven++;
-        return NumberOfEven;
-    }
-    int getNumberOfEven(){
-        return NumberOfEven;
-    }
+struct NameCard{
+    std::string name;
+    int age;
 };
-
+bool condition(NameCard temp1, NameCard temp2){
+    if(temp1.age>temp2.age)
+        return true;
+    else
+        return false;
+}
 int main(){
-    Even even;
-    std::vector<int> v1;
-    srand((unsigned int)time(NULL));
-    int size = rand()%100;
-    for (int i = 0; i < size; ++i){
-        v1.push_back(rand()%100);
+    std::vector<NameCard> v1 = {{"ccc" , 30}, {"aaa" , 10}, {"bbb" , 10}, {"ddd" , 40}, {"eee" , 50}, {"aaa" , 20}};
+    std::vector<NameCard>::iterator it = v1.begin();
+    std::cout<<"정렬 전: ";
+    for( ; it != v1.end() ; it++){
+        std::cout<<"["<<it->name<<", "<<it->age<<"] ";
     }
-    std::vector<int>::iterator it = v1.begin();
-    for ( ; it != v1.end(); ++it){
-        std::cout<<*it<<"..";
-        if(*it%2 == 0)
-            std::cout<<"짝수"<<std::endl;
-        else
-            std::cout<<"홀수"<<std::endl;
-        even(*it);
+    std::cout<<std::endl;
+    std::sort(v1.begin(), v1.end(), condition);
+    it = v1.begin();
+    std::cout<<"정렬 후: ";
+    for( ; it != v1.end() ; it++){
+        std::cout<<"["<<it->name<<", "<<it->age<<"] ";
     }
-    std::cout<<"짝수 개수: "<<even.getNumberOfEven()<<std::endl;
-    return 0;
 }
