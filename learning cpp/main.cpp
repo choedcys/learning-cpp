0fed5d80dbaec16ba528f38677f29d91c2665d31
choedcys <choedcys@naver.com>
2023-01-15 14:57:57 +0900

    람다 문제_8

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,35 +1,19 @@
 #include<iostream>
-#include<vector>
+#include<list>
 
-class Plus{
-public:
-    int operator()(int temp){
-        temp += 100;
-        return temp;
-    }
-};
 
 int main(){
-    Plus p;
-    std::vector<int> v;
-    for(int i = 0 ; i < 5 ; i++){
-        v.push_back((i+1)*10);
-    }
-    std::cout<<"벡터값: ";
-    std::for_each(v.begin(), v.end(), [](int temp){
-        std::cout<<temp<<" ";
-    });
-    std::cout<<std::endl;
-    std::for_each(v.begin(), v.end(), [](int& temp){
-        temp += 100;
-    });
-    std::cout<<"변경 후: ";
-    std::for_each(v.begin(), v.end(), [](int temp){
-        std::cout<<temp<<" ";
-    });
-    std::cout<<std::endl;
-    std::cout<<"변경 후(함수객체): ";
-    for(int i = 0 ; i < 5 ; i++){
-        std::cout<<p(v[i])<<" ";
-    }
+    int start, end;
+    std::cout<<"시작: ";
+    std::cin>>start;
+    std::cout<<"끝: ";
+    std::cin>>end;
+    [](int start, int end){
+        int sum = 0;
+        for(int i = start ; i <= end ; i++){
+            sum+=i;
+        }
+        std::cout<<start<<" ~ "<<end<<"의 합은: "<<sum;
+        std::cout<<std::endl;
+    }(start, end);
 }
