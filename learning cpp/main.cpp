c59d80aff5aa2eac1b8a08b19d75f6442e6dd58a
choedcys <choedcys@naver.com>
2023-01-15 09:00:33 +0900

    람다 문제_6

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,18 +1,35 @@
 #include<iostream>
 #include<vector>
 
+class Plus{
+public:
+    int operator()(int temp){
+        temp += 100;
+        return temp;
+    }
+};
+
 int main(){
-    std::vector<int> v = {34, -43, 56};
-    std::cout<<"변경 전: ";
+    Plus p;
+    std::vector<int> v;
+    for(int i = 0 ; i < 5 ; i++){
+        v.push_back((i+1)*10);
+    }
+    std::cout<<"벡터값: ";
     std::for_each(v.begin(), v.end(), [](int temp){
         std::cout<<temp<<" ";
     });
     std::cout<<std::endl;
     std::for_each(v.begin(), v.end(), [](int& temp){
-        temp *= -1;
+        temp += 100;
     });
     std::cout<<"변경 후: ";
     std::for_each(v.begin(), v.end(), [](int temp){
         std::cout<<temp<<" ";
     });
+    std::cout<<std::endl;
+    std::cout<<"변경 후(함수객체): ";
+    for(int i = 0 ; i < 5 ; i++){
+        std::cout<<p(v[i])<<" ";
+    }
 }
