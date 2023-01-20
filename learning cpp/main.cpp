c17f36212ae5873c70e200d8b0390612ca2cfb0c
choedcys <choedcys@naver.com>
2023-01-20 20:15:34 +0900

    자료구조 재귀문제_19

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,32 +1,16 @@
 #include <iostream>
 
-//int func(int N){
-//    int sum = 0;
-//    sum += N*N*N;
-//    if(N > 1)
-//        return sum+func(N-1);
-//    else
-//        return 1;
-//}
-//
-//int main(){
-//    int N;
-//    std::cin>>N;
-//    std::cout<<func(N);
-//}
-
-int func(int value){
-    if(value == 1){
-        std::cout<<"=";
+int func(int N){
+    if(N > 1){
+        return N*N*N+func(N-1);
+    }
+    else
         return 1;
-        
-    }
-    else{
-        std::cout<<11-value<<"+";
-        return value+func(value-1);
-    }
 }
 
 int main(){
-    std::cout<<func(10);
+    int N;
+    std::cin>>N;
+    std::cout<<func(N)<<std::endl;
 }
+
