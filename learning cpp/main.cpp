12852451db33d73cc8f632fd2c4d72fe8cc9aba8
choedcys <choedcys@naver.com>
2023-01-22 10:50:32 +0900

    자료구조 재귀함수_16

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 
-void show_digit(int N){
+void func(int N){
     if(N == N%10){
         std::cout<<N<<" ";
         return;
     }
     else{
-        show_digit(N/10);
         std::cout<<N%10<<" ";
+        func(N/10);
     }
 }
 
 int main(){
     int N;
     std::cin>>N;
-    show_digit(N);
+    func(N);
     std::cout<<std::endl;
 }
