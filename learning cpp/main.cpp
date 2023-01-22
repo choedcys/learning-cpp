d7a37b9b7d2c4ec3294a665469d69716e26fb50a
choedcys <choedcys@naver.com>
2023-01-22 10:46:27 +0900

    자료구조 재귀문제_17

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,36 +1,19 @@
 #include <iostream>
-void Sharp(int M);
-void Space(int K);
 
-void Shell(int N){
-    if(N==0){
+void show_digit(int N){
+    if(N == N%10){
+        std::cout<<N<<" ";
         return;
     }
     else{
-        Sharp(2*N-1);
-        Space(6-N);
-        Shell(N-1);
-    }
-}
-void Space(int K){
-    if(K == 0)
-        return;
-    else{
-        std::cout<<" ";
-        Space(K-1);
-    }
-}
-void Sharp(int M){
-    if(M==0){
-        std::cout<<std::endl;
-        return;
-    }
-    else{
-        std::cout<<"#";
-        Sharp(M-1);
+        show_digit(N/10);
+        std::cout<<N%10<<" ";
     }
 }
 
 int main(){
-    Shell(5);
+    int N;
+    std::cin>>N;
+    show_digit(N);
+    std::cout<<std::endl;
 }
