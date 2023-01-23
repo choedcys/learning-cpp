fc4f3df4dd82c5943d613530870bbba49232272a
choedcys <choedcys@naver.com>
2023-01-23 22:47:37 +0900

    STL알고리즘 마지막문제

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
-
-void func(int N){
-    if(N == N%10){
-        std::cout<<N<<" ";
-        return;
-    }
-    else{
-        std::cout<<N%10<<" ";
-        func(N/10);
-    }
-}
+#include <vector>
+#include <string>
 
 int main(){
-    int N;
-    std::cin>>N;
-    func(N);
-    std::cout<<std::endl;
+    std::string temp;
+    std::vector<std::string> Man;
+    std::vector<std::string> Woman;
+    std::vector<std::string> Union;
+    std::cout<<"철수친구: ";
+    while(1){
+        getline(std::cin,temp);
+        if(temp == "END")
+            break;
+        else
+            Man.push_back(temp);
+    }
+    std::cout<<"영희친구: ";
+    while(1){
+        getline(std::cin,temp);
+        if(temp == "END")
+            break;
+        else
+            Woman.push_back(temp);
+    }
+    sort(Man.begin(), Man.end());
+    sort(Woman.begin(), Woman.end());
+    std::vector<std::string>::iterator it1 = Man.begin();
+    std::vector<std::string>::iterator it2 = Woman.begin();
+    std::cout<<"철수친구: ";
+    for( ; it1 != Man.end() ; it1++){
+        std::cout<<*it1<<" ";
+    }
+    std::cout<<"영희친구: ";
+    for( ; it2 != Woman.end() ; it2++){
+        std::cout<<*it2<<" ";
+    }
 }
