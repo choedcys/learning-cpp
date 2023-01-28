f8823778cd07c079b5e1983db251c462bff54e40
choedcys <choedcys@naver.com>
2023-01-28 16:12:26 +0900

    자료구조 재귀문제_13

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,29 +1,40 @@
-//#include<iostream>
-//
-//int BinarySearch(){
-//
-//}
-//int main(){
-//    int Key;
-//    int arr[15] = {2,5,34,32,1,4,8,10,23,26,17,19,21,23,6};
-//    std::cin>>
-//}
+#include<iostream>
+#include<algorithm>
 
+int BinarySearch(int *arr, int Key, int end, int start){
+    int mid = (end+start)/2;
+    if(arr[mid] == Key){
+        return mid;
+    }
+    else if(mid - start == 1){
+        return -1;
+    }
+    else if(arr[mid] < Key){
+        return BinarySearch(arr, Key, end, start+mid/2);
+    }
+    else{
+        return BinarySearch(arr, Key, end-mid/2, start);
+    }
+}
 
-#include <iostream>
-
- int Comp(int N,int K){
-     if(K==0){
-         return 1;
-     }
-     else{
-         return N*Comp(N,K-1);
-     }
- }
-
- int main(){
-     int N,K;
-     std::cin>>N>>K;
-     std::cout<<Comp(N,K);
-     std::cout<<std::endl;
- }
+int main(){
+    int Key;
+    int arr[15] = {2,5,34,32,1,4,8,10,23,26,17,19,21,24,6};
+    std::sort(arr,arr+15);
+    for(int i = 0 ; i < 15 ; i++){
+        std::cout<<arr[i]<<" ";
+    }
+    std::cout<<std::endl;
+    for(int i = 0 ; i < 15 ; i++){
+        std::cout<<i<<" ";
+    }
+    std::cout<<std::endl;
+    std::cout<<std::endl;
+    std::cin>>Key;
+    if(BinarySearch(arr, Key, (sizeof(arr)/4)-1,0) != -1){
+        std::cout<<BinarySearch(arr, Key, (sizeof(arr)/4)-1,0)<<std::endl;
+    }
+    else{
+        std::cout<<"그런 값이 없음"<<std::endl;
+    }
+}
