25a47f86d82706c108fec7388f61f7d617d3c494
choedcys <choedcys@naver.com>
2023-01-31 09:04:22 +0900

    단아 수학

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,46 +1,37 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
-int BinarySearch(int *arr, int Key, int end, int start){
-    int mid = (end+start)/2;
-    if(arr[mid] == Key){
-        return mid;
-    }
-    else if(arr[start] == Key){
-        return start;
-    }
-    else if(arr[end] == Key){
-        return end;
-    }
-    else if(mid - start == 1){
-        return -1;
-    }
-    else if(arr[mid] < Key){
-        return BinarySearch(arr, Key, end, start+mid/2);
-    }
-    else{
-        return BinarySearch(arr, Key, end-mid/2, start);
-    }
+int multi(int n){
+    if(n == 0)
+        return 1;
+    else
+        return 10*multi(n-1);
 }
-
 int main(){
-    int Key;
-    int arr[15] = {2,5,34,32,1,4,8,10,23,26,17,19,21,24,6};
-    std::sort(arr,arr+15);
-    for(int i = 0 ; i < 15 ; i++){
-        std::cout<<arr[i]<<" ";
+    int N;
+    int cnt = 1;
+    int sum = 0;
+    int amount[26] = {0};
+    std::string temp;
+    std::vector<std::string> Words;
+    std::cin>>N;
+    std::cin.get();
+    for(int i = 0 ; i < N ; i++){
+        getline(std::cin, temp);
+        Words.push_back(temp);
     }
-    std::cout<<std::endl;
-    for(int i = 0 ; i < 15 ; i++){
-        std::cout<<i<<" ";
+    std::vector<std::string>::iterator it = Words.begin();
+    for(; it != Words.end() ; it++){
+        std::string temp = *it;
+        for(int row = temp.length() ; row > 0 ; row--){
+            amount[temp.at(row)-65] += multi(row);
+        }
     }
-    std::cout<<std::endl;
-    std::cout<<std::endl;
-    std::cin>>Key;
-    if(BinarySearch(arr, Key, (sizeof(arr)/4)-1,0) != -1){
-        std::cout<<BinarySearch(arr, Key, (sizeof(arr)/4)-1,0)<<std::endl;
+    for(int i = 25 ; i >= 0 ; i++){
+        sum += amount[i]*cnt;
+        cnt++;
     }
-    else{
-        std::cout<<"그런 값이 없음"<<std::endl;
-    }
+    std::cout<<sum;
 }
