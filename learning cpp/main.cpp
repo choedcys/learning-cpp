d83744fa76fdbfd2ddfaf57172b3d1f37eb66ebb
choedcys <choedcys@naver.com>
2023-02-02 10:07:45 +0900

    삼성 특강 맞힌문제

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,26 +1,22 @@
 #include<iostream>
 #include<vector>
+
+int fraction(int n1, int n2){
+    if(n2 % n1 == 0)
+        return n1;
+    else
+        return fraction(n2%n1, n1);
+}
+
 int main(){
-    int N,M,sum=0,cnt=0;
-    std::vector<int> vec;
-    std::cin>>N>>M;
-    int *A = new int[N];
-    for(int i = 0 ; i < N ; i++){
-        std::cin>>A[i];
-    }
-    for(int col = 0 ; col < N ; col++){
-        for(int row = col ; row <= N ; row++){
-            for(int val = col ; val < row ; val++){
-                sum+=A[val];
-            }
-            vec.push_back(sum);
-            sum=0;
-        }
-    }
-    std::vector<int>::iterator it = vec.begin();
-    for( ; it!=vec.end();it++){
-        if(M==*it)
-            cnt++;
-    }
-    std::cout<<cnt<<std::endl;
+    int n1, n2, k1, k2;
+    std::cin>>n1>>n2>>k1>>k2;
+    
+    n1 = n1 * k2;
+    k1 = k1 * n2;
+    n2 = n2 * k2;
+    n1 += k1;
+    
+    int div = fraction(n1,n2);
+    std::cout<<n1/div<<" "<<n2/div<<std::endl;
 }
