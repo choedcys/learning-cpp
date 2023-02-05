34a1f6c0b2a79136537d1eb8977095cee6d758ab
choedcys <choedcys@naver.com>
2023-02-05 09:34:51 +0900

    삼성특강1010_시간초과

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,22 +1,32 @@
 #include<iostream>
 #include<vector>
+long long factorial(long long N){
+    long long sum = 1;
+    for(int i = 1 ; i <= N ; i++){
+        sum *= i;
+    }
+    return sum;
+}
 
-int fraction(int n1, int n2){
-    if(n2 % n1 == 0)
-        return n1;
+long long Permutation(long long N, long long M){
+    if(N==1)
+        return M;
     else
-        return fraction(n2%n1, n1);
+        return M*Permutation(N-1, M-1);
 }
 
 int main(){
-    int n1, n2, k1, k2;
-    std::cin>>n1>>n2>>k1>>k2;
-    
-    n1 = n1 * k2;
-    k1 = k1 * n2;
-    n2 = n2 * k2;
-    n1 += k1;
-    
-    int div = fraction(n1,n2);
-    std::cout<<n1/div<<" "<<n2/div<<std::endl;
+    long long T, M, N, *res;
+    std::cin>>T;
+    res = new long long[T];
+    for(int i = 0 ; i < T ; i++){
+        std::cin>>N>>M;
+        if(N > 25 || M > 25)
+            res[i] = Permutation(N-1, M-1)/factorial(N-1) + Permutation(N, M-1)/factorial(N);
+        else
+            res[i] = Permutation(N,M)/factorial(N);
+    }
+    for(int i = 0 ; i < T ; i++){
+        std::cout<<res[i]<<std::endl;
+    }
 }
