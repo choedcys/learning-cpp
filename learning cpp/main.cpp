165a9beb2b6f4df600b15abfea71c3a2bd957074
choedcys <choedcys@naver.com>
2023-02-06 13:52:22 +0900

    삼성특강_집합의표현문제_ios::sync키워드 필수

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,32 +1,47 @@
 #include<iostream>
-#include<vector>
-long long factorial(long long N){
-    long long sum = 1;
-    for(int i = 1 ; i <= N ; i++){
-        sum *= i;
+using namespace std;
+void initialize(int *arr, int n){
+    for(int i = 0 ; i < n+1 ; i++){
+        arr[i] = i;
     }
-    return sum;
 }
 
-long long Permutation(long long N, long long M){
-    if(N==1)
-        return M;
+int find(int *arr, int a){
+    if(arr[a] == a)
+        return a;
     else
-        return M*Permutation(N-1, M-1);
+        return arr[a] = find(arr,arr[a]);
+}
+
+void Union(int *arr, int a, int b){
+    a = find(arr, a);
+    b = find(arr,b);
+    if(a != b){
+        arr[b] = a;
+    }
 }
 
 int main(){
-    long long T, M, N, *res;
-    std::cin>>T;
-    res = new long long[T];
-    for(int i = 0 ; i < T ; i++){
-        std::cin>>N>>M;
-        if(N > 25 || M > 25)
-            res[i] = Permutation(N-1, M-1)/factorial(N-1) + Permutation(N, M-1)/factorial(N);
-        else
-            res[i] = Permutation(N,M)/factorial(N);
-    }
-    for(int i = 0 ; i < T ; i++){
-        std::cout<<res[i]<<std::endl;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n,m;
+    int cal, a,b;
+    cin>>n>>m;
+    int *arr = new int[n+1];
+    initialize(arr, n);
+    for(int i = 0 ; i < m ; i++){
+        std::cin>>cal>>a>>b;
+        if(cal == 0){
+            Union(arr,a,b);
+        }
+        else if(cal == 1){
+            if(find(arr,a)==find(arr,b)){
+                cout<<"YES"<<"\n";
+            }
+            else
+                cout<<"NO"<<"\n";
+        }
     }
 }
