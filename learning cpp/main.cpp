afde9490d4b7e9ecd4646232250a885fd71a8d0e
choedcys <choedcys@naver.com>
2023-03-02 19:39:29 +0900

    자료구조 배열_2

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,47 +1,59 @@
 #include<iostream>
-using namespace std;
-void initialize(int *arr, int n){
-    for(int i = 0 ; i < n+1 ; i++){
-        arr[i] = i;
-    }
-}
-
-int find(int *arr, int a){
-    if(arr[a] == a)
-        return a;
-    else
-        return arr[a] = find(arr,arr[a]);
-}
-
-void Union(int *arr, int a, int b){
-    a = find(arr, a);
-    b = find(arr,b);
-    if(a != b){
-        arr[b] = a;
-    }
-}
-
+//#include<vector>
+//int main(){
+//    int Num, rotate;
+//    int cnt;
+//    std::cin>>Num>>rotate;
+//    int *basket = new int[Num];
+//    for(int i = 0 ; i < Num ; i++){
+//        basket[i] = i+1;
+//    }
+//    for(int cursor = 0 ; cursor < rotate ; cursor++){
+//        int start, end, mid;
+//        std::vector<int>one, two,three;
+//        std::cin>>start>>end>>mid;
+//        for(int i = end+1 ; i < Num+1 ; i++){
+//            three.push_back(basket[i]);
+//        }
+//        for(int i = mid ; i < end ; i++){
+//            two.push_back(basket[i]);
+//        }
+//        for(int i = 0 ; i < mid-end ; i++){
+//            std::cout<<two[i];
+//        }
+//    }
+//}
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    int n,m;
-    int cal, a,b;
-    cin>>n>>m;
-    int *arr = new int[n+1];
-    initialize(arr, n);
-    for(int i = 0 ; i < m ; i++){
-        std::cin>>cal>>a>>b;
-        if(cal == 0){
-            Union(arr,a,b);
-        }
-        else if(cal == 1){
-            if(find(arr,a)==find(arr,b)){
-                cout<<"YES"<<"\n";
-            }
-            else
-                cout<<"NO"<<"\n";
-        }
+    int arr1[6] = {3,6,0,0,0,10};
+    int arr2[6] = {10,4,3,0,0,0};
+    printf("다항식1: ");
+    for(int i = 5 ; i >= 0 ; i--){
+        if(arr1[i] != 0 && (i != 0 && i !=1))
+            printf("%dx^%d+",arr1[i],i);
+        else if(arr1[i] != 0 && i == 1)
+            printf("%dx+",arr1[i]);
+        else if(arr1[i] != 0 && i == 0)
+            printf("%d",arr1[i]);
     }
+    printf("\n");
+    printf("다항식2: ");
+    for(int i = 5 ; i >= 0 ; i--){
+        if(arr2[i] != 0 && (i != 0 && i !=1))
+            printf("%dx^%d+",arr2[i],i);
+        else if(arr2[i] != 0 && i == 1)
+            printf("%dx+",arr2[i]);
+        else if(arr2[i] != 0 && i == 0)
+            printf("%d",arr2[i]);
+    }
+    printf("\n");
+    printf("다항식합: ");
+    for(int i = 5 ; i >= 0 ; i--){
+        if(arr1[i]+arr2[i] != 0 && (i != 0 && i !=1))
+            printf("%dx^%d+",arr1[i]+arr2[i],i);
+        else if(arr1[i]+arr2[i] != 0 && i == 1)
+            printf("%dx+",arr1[i]+arr2[i]);
+        else if(arr1[i]+arr2[i] != 0 && i == 0)
+            printf("%d",arr1[i]+arr2[i]);
+    }
+    printf("\n");
 }
