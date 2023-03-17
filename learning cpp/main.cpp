cd27781a993fbf93885e822f52b39702f61c46e7
choedcys <choedcys@naver.com>
2023-03-17 20:22:46 +0900

    학교 알고리즘과제_파이썬 리스트를 C로 구현

--- learning cpp/main.cpp
+++ learning cpp/main.cpp
@@ -1,124 +1,153 @@
-#include <iostream>
-#include <vector>
 
-void show(int temp[][6], int N){
-    for(int col = 0 ; col < N ; col++){
-        for(int row = 0 ; row < 6 ; row++){
-            std::cout<<temp[col][row]<<" ";
-        }
-        std::cout<<std::endl;
-    }
+#include<stdio.h>
+#include<stdlib.h>
+#define STACK_SIZE 10
+void printStack();
+
+int *stack;
+int top = -1;
+int size = 1;
+
+bool isEmpty(){
+    if(top == -1)
+        return 1;
+    else
+        return 0;
 }
-void show(int temp[][3], int N){
-    for(int col = 0 ; col < N ; col++){
-        for(int row = 0 ; row < 3 ; row++){
-            std::cout<<temp[col][row]<<" ";
+bool isFull(){
+    if(top % STACK_SIZE == 9)
+        return 1;
+    else
+        return 0;
+}
+
+void insert(int pos, int value){
+    int *temp;
+    if(pos > top){
+        printf("Cannot execute pop because index exceed STACK RANGE\n");
+    }
+    else if(isFull()){
+        printf("Full STACK\n");
+        temp = new int[STACK_SIZE*size];
+
+        for(int i = 0 ; i < (top + 1) ; i++)
+            temp[i] = stack[i];
+        stack = new int[STACK_SIZE*(size+1)];
+
+        for(int i = 0 ; i < pos ; i++)
+            stack[i] = temp[i];
+        stack[pos] = value;
+        for(int i = pos+1 ; i < top+2 ; i++)
+            stack[i] = temp[i-1];
+
+        size++;
+        top++;
+        printf("Insert Completed!\n");
+        free(temp);
+    }
+    else{
+        printf("Enough Space\n");
+        temp = new int[top-pos];
+        for(int i = pos ; i < top+1 ; i++){
+            temp[i-pos] = stack[i];
         }
-        std::cout<<std::endl;
+        stack[pos] = value;
+        for(int i = pos+1 ; i < top+2 ; i++){
+            stack[i] = temp[i-pos-1];
+        }
+        top++;
+        printf("Insert Completed!\n");
+        free(temp);
+    }
+
+
+
+}
+
+int pop(int index){//사이즈도 줄여야함
+    int val;
+    if(isEmpty()){
+        printf("Cannot execute pop becaseof Empty STACK\n");
+        return -1;
+    }
+    else if(top < index+1){
+        printf("Cannot execute pop because index exceed STACK RANGE\n");
+        return -1;
+    }
+    else{
+        val = stack[index];
+        for(int i = index ; i < top+1 ; i++){
+            stack[i] = stack[i+1];
+        }
+        top--;
+        if(top%10 == 0){
+            printf("실행됨");
+            size--;
+            int *temp = new int[STACK_SIZE*(size)];
+            for(int i = 0 ; i < top ; i++){
+                temp[i] = stack[i];
+            }
+            stack = new int[STACK_SIZE*(size)];
+            for(int i = 0 ; i < top ; i++){
+                stack[i] = temp[i];
+            }
+            delete []temp;
+        }
+        return val;
     }
 }
 
-int rare(int (*temp)[6], int N, int (*rarearr)[3]){
-    int num = 0;
-    for(int col = 0 ; col < N ; col++){
-        for(int row = 0 ; row < 6 ; row++){
-            if(temp[col][row] != 0)
-                num++;
-        }
+void push(int item){
+    int *temp;
+
+    if(isFull()){
+        printf("Full STACK\n");
+        temp = (int*)malloc((STACK_SIZE*size)*sizeof(int));
+
+        for(int i = 0 ; i < (top + 1) ; i++)
+            temp[i] = stack[i];
+        stack = (int*)malloc((STACK_SIZE*(size+1))*sizeof(int));
+
+        for(int i = 0 ; i < (top + 1) ; i++)
+            stack[i] = temp[i];
+        stack[++top] = item;
+
+        size++;
+        free(temp);
     }
-    num = 0;
-    for(int col = 0 ; col < N ; col++){
-        for(int row = 0 ; row < 6 ; row++){
-            if(temp[col][row] != 0){
-                rarearr[num][0] = col;
-                rarearr[num][1] = row;
-                rarearr[num][2] = temp[col][row];
-                num++;
-            }
-        }
-    }
-    return num;
+    else
+        stack[++top] = item;
 }
-int Integrated(int (*A)[3], int (*B)[3], int(*Rare)[3], int numA, int numB){
-    int IndexOfA = 0, IndexOfB = 0, IndexOfRare = 0, DetOfSum = 0;
-    while(IndexOfA != numA && IndexOfB != numB){
-        for(int i = 0 ; i < 3 ; i++){
-            if(DetOfSum == 2){
-                for(int j = 0 ; j < 2 ; j++){
-                    Rare[IndexOfRare][j] = B[IndexOfB][j];
-                }
-                Rare[IndexOfRare][2] = A[IndexOfA][2]+B[IndexOfB][2];
-                IndexOfRare++;
-                IndexOfA++;
-                IndexOfB++;
-                DetOfSum = 0;
-                break;
-            }
-            if(A[IndexOfA][i] < B[IndexOfB][i]){
-                for(int j = 0 ; j < 3 ; j++){
-                    Rare[IndexOfRare][j] = A[IndexOfA][j];
-                }
-                IndexOfRare++;
-                IndexOfA++;
-                break;
-            }
-            else if(A[IndexOfA][i] > B[IndexOfB][i]){
-                for(int j = 0 ; j < 3 ; j++){
-                    Rare[IndexOfRare][j] = B[IndexOfB][j];
-                }
-                IndexOfRare++;
-                IndexOfB++;
-                break;
-            }
-            else
-                DetOfSum++;
-        }
-        DetOfSum = 0;
+
+void printStack(){
+    for(int i = 0 ; i < top+1 ; i++){
+        if(i % 10 == 0)
+            printf("\n");
+        printf("%d ",stack[i]);
     }
-    return IndexOfRare;
-}
-void Convert(int (*Ans)[6], int(*Target)[3], int numRare){
-    for(int row = 0 ; row < numRare ; row++){
-        for(int col = 0 ; col < 3 ; col++){
-            int x, y;
-            if(col == 0){
-                x = Target[row][col];
-            }
-            else if(col == 1){
-                y = Target[row][col];
-            }
-            else
-                Ans[x][y] = Target[row][col];
-        }
-    }
+    printf("\n\n");
 }
 
 int main(){
-    int A[6][6] = {{0,0,0,7,0,0}, {9,0,0,0,0,8}, {0,0,0,0,0,0}, {6,5,0,0,0,0}, {0,0,0,0,0,1}, {0,0,2,0,0,0}};
-    int B[6][6] = {{3,0,0,1,2,0}, {0,8,0,0,5,2}, {2,1,1,0,3,4}, {5,0,0,9,0,0}, {2,1,1,0,0,5}, {6,0,4,0,0,0}};
-    int rareA[100][3];
-    int rareB[100][3];
-    int Rare[200][3];
-    int AnswerMatrix[6][6] = {0};
-    show(A,6);
-    std::cout<<std::endl;
-    show(B,6);
-    std::cout<<std::endl;
+    int i;
+    //stack = (int*)malloc(STACK_SIZE*sizeof(int));
+    stack = new int[STACK_SIZE];
+    for(i = 0 ; i < 14 ; i++)
+        push(i+1);
+    printStack();
+    for(i = 0 ; i < 7 ; i++){
+        printf("POP Data[%d] = %d >>>> %d\n",i+1,pop(i),size);
+        printStack();
+    }
 
-    int numA = rare(A,6, rareA);
-    int numB = rare(B,6, rareB);
+    free(stack);
 
-    std::cout<<"0제거 A"<<std::endl;
-    show(rareA,numA);
-    std::cout<<"0제거 B"<<std::endl;
-    show(rareB,numB);
-    int numRare = Integrated(rareA, rareB, Rare, numA, numB);
-    std::cout<<"AB통합"<<std::endl;
-    show(Rare,numRare);
-    Convert(AnswerMatrix, Rare, numRare);
-    std::cout<<"답안"<<std::endl;
-    show(AnswerMatrix,6);
 }
+
+
+
+
+
 
 
 
